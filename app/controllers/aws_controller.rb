7b27923507d040d0834b4f9bfef84602afc705ee
Taylor Daugherty <mdaugherty6@gmail.com>
2015-11-24 15:37:12 -0500

    mapped prefixes in aws_controller

--- app/controllers/aws_controller.rb
+++ app/controllers/aws_controller.rb
@@ -9,6 +9,6 @@ class AwsController < ApplicationController
     prefix = params[:prefix] + '/'
     objects = AWS.list_objects(bucket: 'coulter-api-dev', prefix: prefix,  delimiter: '/');
     @contents = objects.contents.map(&:key)
-    @directories = objects.common_prefixes
+    @directories = objects.common_prefixes.map(&:prefix)
   end
 end
