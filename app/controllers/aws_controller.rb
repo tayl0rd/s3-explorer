f9a10d9377447c35263f2a8a25ae6446520ecdbb
Taylor Daugherty <mdaugherty6@gmail.com>
2015-11-24 16:05:41 -0500

    directory linking

--- app/controllers/aws_controller.rb
+++ app/controllers/aws_controller.rb
@@ -2,7 +2,7 @@ class AwsController < ApplicationController
   def index
     objects = AWS.list_objects(bucket: 'coulter-api-dev', delimiter: '/');
     @contents = objects.contents.map(&:key)
-    @directories = objects.common_prefixes
+    @directories = objects.common_prefixes.map(&:prefix)
   end
 
   def subdirectory
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  BASE_URL = "https://s3-us-west-2.amazonaws.com/coulter-api-dev/"
 end
